408c787ea14c4c2d8d5059271515d4d3ed6bfd68
Nguyễn Thành <thanhngune123@gmail.com>
2025-10-05 02:18:39 +0700

    Deploy Mini Dice Game to GitHub Pages

--- assets/scripts/core/RandomService.ts
+++
@@ -1,9 +0,0 @@
-export type RandFn = () => number;
-
-export class RandomService {
-  constructor(private randFn: RandFn = Math.random) {}
-
-  randInt(min: number, max: number): number {
-    return Math.floor(this.randFn() * (max - min + 1)) + min;
-  }
-}
--- tests/diceService.test.ts
+++
@@ -1,9 +0,0 @@
-export class DiceService {
-  roll(): number[] {
-    return [
-      1 + Math.floor(Math.random() * 6),
-      1 + Math.floor(Math.random() * 6),
-      1 + Math.floor(Math.random() * 6),
-    ];
-  }
-}
